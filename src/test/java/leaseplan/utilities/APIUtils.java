049170e841579e069d30a08c8f2b456ebd981c4e
Hüseyin Yıldırım <11huseyinyildirim@gmail.com>
2023-06-04 11:39:56 +0200

    Three more POJO classes have been added, and an additional method has been added to API.Utils.

--- src/test/java/leaseplan/utilities/APIUtils.java
+++ src/test/java/leaseplan/utilities/APIUtils.java
@@ -1,16 +1,40 @@
 package leaseplan.utilities;
 
+import io.cucumber.java.it.Ma;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import leaseplan.pojo.*;
 
 import java.util.List;
+import java.util.Map;
 
 public class APIUtils {
 
 
+
     /**
-     * This class gets all colas and stores them inside a List<Cola> object
+     * This method accepts one argument which should be a valid endpoint
+     * and stores all the response body inside a  List<Map<String, Object>> object
+     * @return
+     */
+
+    public static List<Map<String,Object>> getsAllObjects(String endPoint){
+
+        if (!(endPoint.equals("cola") || endPoint.equals("orange")
+                || endPoint.equals("pasta") || endPoint.equals("apple"))) {
+            throw new RuntimeException("Invalid endpoint");
+        }
+
+        Response response = RestAssured.get("https://waarkoop-server.herokuapp.com/api/v1/search/demo/" +endPoint );
+        List<Map<String, Object>> allObject = response.body().as(List.class);
+
+
+        return allObject;
+
+    }
+
+    /**
+     * This method gets all colas and stores them inside a List<Cola> object
      * @return
      */
     public static List<Cola> getAllColas() {
@@ -23,7 +47,7 @@ public class APIUtils {
 
 
     /**
-     * This class gets all colas and stores them inside a List<Apple> object
+     * This method gets all colas and stores them inside a List<Apple> object
      * @return
      */
     public static List<Apple> getAllApples() {
@@ -36,7 +60,7 @@ public class APIUtils {
 
 
     /**
-     * This class gets all oranges and stores them inside a List<Orange> object
+     * This method gets all oranges and stores them inside a List<Orange> object
      * @return
      */
     public static List<Orange> getAllOrange() {
@@ -50,7 +74,7 @@ public class APIUtils {
 
 
     /**
-     * This class gets all pastas and stores them inside a List<Pasta> object
+     * This method gets all pastas and stores them inside a List<Pasta> object
      * @return
      */
     public static List<Pasta> getAllPasta() {
